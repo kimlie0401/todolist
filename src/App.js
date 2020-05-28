a65a99c1fe646126664f72743abcb23786f7ff98
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-05-28 16:50:08 -0700

    # Setup Context API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoHeader from "./components/TodoHeader";
 import TodoList from "./components/TodoList";
 import TodoCreate from "./components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
 const GlobalStyle = createGlobalStyle`
   body{
@@ -14,14 +15,14 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHeader />
         <TodoList />
         <TodoCreate />
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
